4d3ef0dc9ae502a955eadba3bd4972f4b7d00fab
Boyi Qu <boyiqu9596@gmail.com>
2022-10-13 10:31:55 -0700

    fixed import reviews in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Product from './po_components/Product.jsx';
 import RelatedItems from './ri_components/RelatedItems.jsx';
-import Reviews.jsx from './rr_components/Reviews.jsx';
+import Reviews from './rr_components/Reviews.jsx';
 
 
 //I set up three different folders for each widget: product overview (po), related items(ri) and ratings and reviews (rr). Each contains a main div, which will be a child to App. you guys can rename the components and folder if you guys want.
