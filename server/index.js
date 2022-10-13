b6d070c567d354d65524e8799a5aa60ba7e1f263
Connor Fung <cmfung23@gmail.com>
2022-10-12 17:27:10 -0700

    to set up server/index.js

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path')
+const axios = require('axios');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+// middleware
+
+// logging and parsing
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json());
+
+// serve up client files
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// routes/controllers will utilize client requests and communicate with the API here.
+
+const port = 3000;
+
+app.listen(port);
+console.log(`server listening at http://localhost:${port}`)
