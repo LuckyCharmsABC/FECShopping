ad037b1a069ac55fca20e18976f6032e44049c05
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-10-12 17:10:47 -0700

    added the plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "client/src", "index.js")
+  mode: "development",
+  entry: path.join(__dirname, "client/src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "client/dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "client/dist",)
+    })
+  ]
 };
