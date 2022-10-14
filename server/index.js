23dd31402478190ae015de22116972760d493a1c
Connor Fung <cmfung23@gmail.com>
2022-10-14 10:26:47 -0700

    to add linter config file and fix some linting errors.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const axios = require('axios');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -10,16 +10,16 @@ const app = express();
 // middleware
 
 // logging and parsing
-app.use(cors())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 // serve up client files
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // routes/controllers will utilize client requests and communicate with the API here.
 
 const port = process.env.PORT || 3000;
 
 app.listen(port);
-console.log(`server listening at http://localhost:${port}`)
+console.log(`server listening at http://localhost:${port}`);
