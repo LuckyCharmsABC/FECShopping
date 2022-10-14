3e9071d78f234e0f5e7ae77269c3f219f6c169d5
Boyi Qu <boyiqu9596@gmail.com>
2022-10-14 12:26:43 -0700

    changed the spec file to include describe

--- client/lib/react-specs.test.js
+++ client/lib/react-specs.test.js
@@ -2,12 +2,16 @@ import React from 'react';
 // import react-testing methods
 import {render, fireEvent, waitFor, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from '..src/components/App.jsx';
+import App from '../src/components/App.jsx';
 
-test('Displays Hello in App', async()=>{
-  render(<App />)
 
-  await waitFor(()=> screen.getByRole('heading'))
+describe('Example test', ()=>{
 
-  expect(screen.getByRole('heading')).toHaveTextContent('Hello')
+  test('Displays Hello in App', async()=>{
+    render (<App />)
+
+    await waitFor(()=> screen.getByRole('heading'))
+
+    expect(screen.getByRole('heading')).toHaveTextContent('Hello')
+  })
 })
