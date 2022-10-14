d1119f9f03328fbac040a0bb680e0455110c9cf3
Boyi Qu <boyiqu9596@gmail.com>
2022-10-14 16:22:43 -0700

    finalized the front end test file for the example test

--- client/lib/react-specs.test.js
+++ client/lib/react-specs.test.js
@@ -1,17 +1,17 @@
 import React from 'react';
 // import react-testing methods
-import {render, fireEvent, waitFor, screen} from '@testing-library/react';
+import {render, waitFor, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../src/components/App.jsx';
 
 
+
 describe('Example test', ()=>{
-
-  test('Displays Hello in App', async()=>{
+  test('Displays Hello in App', ()=>{
     render (<App />)
-
-    await waitFor(()=> screen.getByRole('heading'))
-
-    expect(screen.getByRole('heading')).toHaveTextContent('Hello')
+    waitFor(()=> screen.getByRole('heading'))
+    .then(()=>{
+      expect(screen.getByRole('heading')).toHaveTextContent('Hello')
+    })
   })
 })
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ const config = {
   verbose: true,
   testEnvironment: 'jsdom',
 };
-console.log('picking up config')
+//console.log('picking up config')
 module.exports = config;
