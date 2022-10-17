7d56e83f96bdbdcc5502d2f3b1da36155d2ea8de
Boyi Qu <boyiqu9596@gmail.com>
2022-10-17 16:34:02 -0700

    set up StyleSelector component, rendering each component in a table

---
+++ client/src/components/po_components/StyleSelector.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+const StyleSelector = ( {product} ) =>(
+  <div>
+    <p>
+      <b>STYLE</b>
+    </p>
+    <table className="styleTable">
+      <tbody>
+        <tr>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+        </tr>
+        <tr>
+          <th>5</th>
+          <th>6</th>
+          <th>7</th>
+          <th>8</th>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+);
+
+export default StyleSelector;
