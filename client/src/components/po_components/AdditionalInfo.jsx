43c6f900db6cf213bf6020ad60802fd38d7c4df0
Boyi Qu <boyiqu9596@gmail.com>
2022-10-17 16:16:13 -0700

    reformatted to adapt to linter rules

--- client/src/components/po_components/AdditionalInfo.jsx
+++ client/src/components/po_components/AdditionalInfo.jsx
@@ -1,14 +1,10 @@
 import React, {useState, useEffect} from 'react';
 
-const AdditionalInfo = ({product}) => {
-  return (
-    <div>
-      <b>{product.slogan}</b>
-      <p>{product.description}</p>
-    </div>
-  )
-}
+const AdditionalInfo = ({ product }) => (
+  <div>
+    <b>{product.slogan}</b>
+    <p>{product.description}</p>
+  </div>
+);
 
 export default AdditionalInfo;
-
-
--- client/src/components/po_components/Gallery.jsx
+++ client/src/components/po_components/Gallery.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 
-const Gallery = ({product}) => (
+const Gallery = ({ product }) => (
   <div>
     <p>image placeholder</p>
   </div>
-)
+);
 
 export default Gallery;
