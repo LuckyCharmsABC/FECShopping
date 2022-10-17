d27fa080d95db7a8cf4b9553a4357c48a76d8c12
Boyi Qu <boyiqu9596@gmail.com>
2022-10-17 16:16:46 -0700

    reformatted to adapt to linter rules

--- client/src/components/po_components/Info.jsx
+++ client/src/components/po_components/Info.jsx
@@ -1,15 +1,12 @@
 import React, {useState, useEffect} from 'react';
 
-const Info = ({product}) => {
-
-  return (
-    <div id="productInfo">
-      <p>Reviews</p>
-      <p>CATAGORY</p>
-      <b>{product.name}</b>
-      <p>${product.default_price}</p>
-    </div>
-  )
-};
+const Info = ({ product }) => (
+  <div id="productInfo">
+    <p>Reviews</p>
+    <p>CATAGORY</p>
+    <b>{product.name}</b>
+    <p>${product.default_price}</p>
+  </div>
+);
 
 export default Info;
