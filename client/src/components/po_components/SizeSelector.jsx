7ad9124fa90d14e3e494deebfbbbabe4ff1da5d9
Boyi Qu <boyiqu9596@gmail.com>
2022-10-18 15:16:26 -0700

    SizeSelector renders correctly, quantity reflects the selected size

--- client/src/components/po_components/SizeSelector.jsx
+++ client/src/components/po_components/SizeSelector.jsx
@@ -1,18 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
 const SizeSelector = ({ productSkus }) => {
-  const [selectedSize, setSelectedSize] = useState('');
+  console.log(productSkus);
+  const [maxQuant, setMaxQuant] = useState(0);
+  const renderQuantOpts = (value) => {
+    const options = [];
+    for (let i = 1; i <= value; i += 1) {
+      options.push(<option>{i}</option>);
+    }
+    return options;
+  };
 
   return (
     <div className="selectors">
-      <select id="sizeSelector">
-        <option value="Select Size">Select Size</option>
-        {productSkus.map((sku) => (
-          <option>{sku.size}</option>
+      <select
+        id="sizeSelector"
+        onChange={(event) => {
+          setMaxQuant(event.target.value < 15 ? event.target.value : 15);
+        }}
+      >
+        <option value="0">Select Size</option>
+        {productSkus.map((sku, index) => (
+          <option value={productSkus[index].quantity}>{sku.size}</option>
         ))}
       </select>
       <select id="numSelector">
-        <option>1</option>
+        {renderQuantOpts(maxQuant)}
       </select>
     </div>
   );
