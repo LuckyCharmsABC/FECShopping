b5225afccfe6fe5f9b68edec18ddad92b3eaa268
Connor Fung <cmfung23@gmail.com>
2022-10-18 12:57:39 -0700

    to stage changes to components before merging Boyi's changes

--- client/src/components/ri_components/RelatedItemsAndOutfits.jsx
+++ client/src/components/ri_components/RelatedItemsAndOutfits.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import RelatedList from './RelatedList.jsx'
-import Outfits from './OutfitList.jsx'
+import React, { useState } from 'react';
+import axios from 'axios';
+import RelatedList from './RelatedList.jsx';
+import Outfits from './OutfitList.jsx';
 
-const RelatedItemsAndOutfits = (props) => (
-  <div>
+const RelatedItemsAndOutfits = ({ currentItem, setCurrentItem }) => {
+  const [relatedItems, setRelatedItems] = useState([]);
+  axios.get(`/products/${currentItem.id}/related`)
+    .then((results) => {
+      console.log(results.data);
+      // setRelatedItems(results.data);
+    })
+    .catch((err) => console.log(err));
+  return (
     <div>
-      <RelatedList />
+      <div>
+        <RelatedList />
+      </div>
+      <div>
+        <Outfits />
+      </div>
     </div>
-    <div>
-      <Outfits />
-    </div>
-  </div>
-)
+  );
+};
 
 export default RelatedItemsAndOutfits;
--- client/src/components/ri_components/RelatedList.jsx
+++ client/src/components/ri_components/RelatedList.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import RelatedItem from './RelatedItem.jsx'
 
 const RelatedList = () => {
+  asdasd
   return (
     <div>
       <h3>Related Items</h3>
-      <RelatedItem />
+      <ul>
+        <RelatedItem />
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default RelatedList;
