7790ab6fb76f0ccbd5e765af8f015d461f3fda00
Boyi Qu <boyiqu9596@gmail.com>
2022-10-17 23:30:13 -0700

    added StyleSelector component, rendering data from productstyle

--- client/src/components/po_components/StyleSelector.jsx
+++ client/src/components/po_components/StyleSelector.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-const StyleSelector = ( {product} ) =>(
-  <div>
-    <p>
-      <b>STYLE</b>
-    </p>
-    <table className="styleTable">
-      <tbody>
-        <tr>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-          <th>4</th>
-        </tr>
-        <tr>
-          <th>5</th>
-          <th>6</th>
-          <th>7</th>
-          <th>8</th>
-        </tr>
-      </tbody>
-    </table>
 
-  </div>
-);
+const StyleSelector = ({ productStyles }) => {
+  if (productStyles.length === 0) {
+    console.log('productStyles in component is ', productStyles);
+    return <div>Loading</div>;
+  }
+  return (
+    <div>
+      <p>
+        <b>STYLE</b>
+      </p>
+      <div className="styleList">
+        {productStyles.map((style)=> (
+          <div><img width="50" height="70" alt="x" src={style.photos[0].thumbnail_url} /></div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default StyleSelector;
