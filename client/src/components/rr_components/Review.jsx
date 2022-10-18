db7cdb15cb2d3d9df3ae32314ea3b9ef03b8b069
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-10-18 15:29:32 -0700

    installed date-fns and applied it to the reviews

--- client/src/components/rr_components/Review.jsx
+++ client/src/components/rr_components/Review.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { format, parseISO } from 'date-fns';
 
 const Review = ({ review }) => {
   const recommended = review.recommend ? <h5>I recommend this product</h5> : <div />;
+  const date = parseISO(review.date);
+
   return (
     <div>
       <h4>{review.reviewer_name}</h4>
-      <h5>{review.date}</h5>
+      <h5>{format(date, 'PPP')}</h5>
       <h5>
         {review.rating}
         stars
