18ae51a8609fe108e9f02434edb932a41a26721d
Boyi Qu <boyiqu9596@gmail.com>
2022-10-18 10:50:08 -0700

    modified cart component where a child component SizeSelector is added

--- client/src/components/po_components/Cart.jsx
+++ client/src/components/po_components/Cart.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
+import SizeSelector from './SizeSelector.jsx';
 
-const Cart = ({ product }) => (
+const Cart = ({ productStyles }) => (
   <div id="cartSection">
-    <div className="selectors">
-      <select>
-        <option value="Select Size">Select Size</option>
-        <option value="S">S</option>
-      </select>
-      <select>
-        <option value="1">1</option>
-        <option value="2">2</option>
-      </select>
-    </div>
+    <SizeSelector productStyles={productStyles} />
     <div>
       <button type="submit">ADD TO BAG</button>
       <button type="submit">⭐️</button>
     </div>
   </div>
+
 );
 export default Cart;
