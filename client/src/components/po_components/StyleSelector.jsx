232f3c56b4e4370e455c869d74fba2503f61e472
Boyi Qu <boyiqu9596@gmail.com>
2022-10-18 10:49:16 -0700

    StyleSelector renders correctly

--- client/src/components/po_components/StyleSelector.jsx
+++ client/src/components/po_components/StyleSelector.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
-const StyleSelector = ({ productStyles }) => {
-  if (productStyles.length === 0) {
-    return <div>Loading</div>;
-  }
-  return (
-    <div>
-      <p>
-        <b>STYLE</b>
-      </p>
-      <div className="styleList">
-        {productStyles.map((style) => (
-          <div><img width="50" height="70" alt="x" src={style.photos[0].thumbnail_url} /></div>
-        ))}
-      </div>
+const StyleSelector = ({ productStyles }) => (
+  <div>
+    <p>
+      <b>STYLE</b>
+    </p>
+    <div className="styleList">
+      {productStyles.map((style) => (
+        <div key={style.style_id}><img width="50" height="70" alt="x" src={style.photos[0].thumbnail_url} /></div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default StyleSelector;
