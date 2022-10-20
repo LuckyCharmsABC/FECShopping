0764b3892a31c260feea1a1e04e4d246a377abe3
Boyi Qu <boyiqu9596@gmail.com>
2022-10-19 18:48:54 -0700

    price now shows sale and original line through prices

--- client/src/components/po_components/Info.jsx
+++ client/src/components/po_components/Info.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 const Info = ({ product, selectedStyle }) => {
   console.log('selectedStye passed in to info is ', selectedStyle);
-
+  const saleStyle = { color: 'red' };
+  const saleOriginal = { 'text-decoration': 'line-through' };
   return (
     <div id="productInfo">
       <p>Read all reviews</p>
       <p>CATAGORY</p>
       <b>{product.name}</b>
-      <p>
+      <div style={selectedStyle.sale_price ? saleStyle : { color: 'black' }}>
         $
-        {selectedStyle.original_price}
-      </p>
+        { selectedStyle.sale_price ? selectedStyle.sale_price : selectedStyle.original_price }
+      </div>
+      <div style={selectedStyle.sale_price ? saleOriginal : { color: 'black' }}>
+        {selectedStyle.sale_price ? selectedStyle.original_price : ''}
+      </div>
     </div>
   );
 };
