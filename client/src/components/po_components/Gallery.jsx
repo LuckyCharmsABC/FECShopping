5680ad113488a9431386a496ecea7f41ade334ff
Boyi Qu <boyiqu9596@gmail.com>
2022-10-22 16:19:32 -0700

    added images and example styling; buttons are not correctly positioned

--- client/src/components/po_components/Gallery.jsx
+++ client/src/components/po_components/Gallery.jsx
@@ -20,27 +20,29 @@ const Gallery = ({ selectedStyle, selectedImageIndex, changeSelectedImgInx }) =>
           </div>
         ))}
       </div>
-      <div className="dcarousel__viewport">
-        {selectedStyle.photos.map((photo, index) => {
-          const { length } = selectedStyle.photos;
-          const slideId = `slide${index + 1}`;
-          const prevHref = index === 0 ? `#carousel_slide${length}` : `#carousel_slide${index}`;
-          const nextHref = index === length - 1 ? `#carousel_slide${0}` : `#carousel_slide${index + 2}`;
-          return (
-            <li
-              id={slideId}
-              tabIndex="0"
-              className="carousel__slide"
-            >
-{/*               <img className="displayedImage" alt="x" src={photo.url} /> */}
-              <div className="carousel__snapper">
-                <a href={prevHref} className="carousel__prev">Go to last slide</a>
-                <a href={nextHref} className="carousel__next">Go to next slide</a>
-              </div>
-            </li>
-          );
-        })}
-      </div>
+      <section className="carousel">
+        <ol className="carousel__viewport">
+          {selectedStyle.photos.map((photo, index) => {
+            const { length } = selectedStyle.photos;
+            const slideId = `carousel_slide${index + 1}`;
+            const prevHref = index === 0 ? `#carousel_slide${length}` : `#carousel_slide${index}`;
+            const nextHref = index === length - 1 ? `#carousel_slide${0}` : `#carousel_slide${index + 2}`;
+            return (
+              <li
+                id={slideId}
+                tabIndex="0"
+                className="carousel__slide"
+              >
+                <img className="displayedImage" alt="x" src={photo.url} />
+                <div className="carousel__snapper">
+                  <a href={prevHref} className="carousel__prev">Go to last slide {prevHref}</a>
+                  <a href={nextHref} className="carousel__next">Go to next slide {nextHref}</a>
+                </div>
+              </li>
+            );
+          })}
+        </ol>
+      </section>
 {/*       <div id="displayBkg">
         <img className="displayedImage" alt="x" src={selectedStyle.photos[selectedImageIndex].url} />
       </div> */}
