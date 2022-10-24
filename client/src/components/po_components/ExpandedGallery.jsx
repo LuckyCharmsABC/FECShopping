29bd5bcaa06f380e466c58e785e0438ede8f5a79
Boyi Qu <boyiqu9596@gmail.com>
2022-10-24 14:48:35 -0700

    added expandedGallery component gallery section and styled

---
+++ client/src/components/po_components/ExpandedGallery.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+const ExpandedGallery = ({ selectedStyle, selectedImageIndex, changeSelectedImgInx }) => {
+  const selectedCss = { border: 'solid black' };
+  const [url, setUrl] = useState(window.location.href);
+
+  return (
+    <div id="expanded-gallery">
+{/*       <div>Expanded view</div>
+      <div id="gallery">
+        {selectedStyle.photos.map((photo, index) => {
+          const jumpSlideId = `#s${index + 1}`;
+          return (
+            <div
+              className="thumbnail"
+              key={photo.url}
+              role="button"
+              onClick={() => { changeSelectedImgInx(index); }}
+              onKeyPress={() => {}}
+              tabIndex={0}
+              style={index === selectedImageIndex ? selectedCss : { border: 'solid white' }}
+            >
+              <a
+                href={jumpSlideId}
+              >
+                <img width="85" height="auto" alt="x" src={photo.thumbnail_url} />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+ */}
+      <section className="expanded-slider">
+        <div>
+          <ul id="expanded-s">
+            {selectedStyle.photos.map((photo, index) => {
+              const { length } = selectedStyle.photos;
+              const slideId = `s${index + 1}`;
+              const prev = `#s${index}`;
+              const next = `#s${index + 2}`;
+              return (
+                <li id={slideId} className="expanded-slide">
+                  <img className="expanded-displayed" src={photo.url} alt="x" />
+                  <div className="expanded-snapper">
+                    <a
+                      className={index === 0 ? 'nav-button disabled' : 'nav-button'}
+                      href={prev}
+                    >
+                      <i
+                        className={index === 0 ? "fa-solid fa-chevron-left fa-xl disabled-nav-button" : 'fa-solid fa-chevron-left fa-xl'}
+                        onClick={()=>{ changeSelectedImgInx(index - 1)}}
+                      />
+                    </a>
+                    <a
+                      className={index === length - 1 ? 'nav-button disabled' : 'nav-button'}
+                      href={next}
+                    >
+                      <i
+                        className={index === length - 1 ? "fa-solid fa-chevron-right fa-xl disabled-nav-button" : 'fa-solid fa-chevron-right fa-xl'}
+                        onClick={()=>{ changeSelectedImgInx(index + 1)}}
+                      />
+                    </a>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+      </section>
+    </div>
+  );
+};
+
+export default ExpandedGallery;
