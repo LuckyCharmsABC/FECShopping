6724af2a45f110fd9747d2b00219ae2116e36a0a
Boyi Qu <boyiqu9596@gmail.com>
2022-10-24 23:04:17 -0700

    added zoom in functionality to expandedView

--- client/src/components/po_components/ExpandedGallery.jsx
+++ client/src/components/po_components/ExpandedGallery.jsx
@@ -7,63 +7,23 @@ const ExpandedGallery = ({ selectedStyle, selectedImageIndex, changeSelectedImgI
 
   return (
     <div id="expanded-gallery">
-      <div onClick={() => {changeView(true)}}>
-        <img className="expanded-displayed-image" src={selectedStyle.photos[selectedImageIndex].url} alt="x" />
-      </div>
-{/*       <section className="expanded-slider">
-        <div>
-          <ul id="expanded-s">
-            {selectedStyle.photos.map((photo, index) => {
-              const { length } = selectedStyle.photos;
-              const slideId = `s${index + 1}`;
-              const prev = `#s${index}`;
-              const next = `#s${index + 2}`;
-              return (
-                <li
-                  id={slideId}
-                  className="expanded-slide"
-                  onClick={(event)=> {
-                  event.preventDefault();
-                  if (!zoomedIn) {
-                    setZoomedIn(true);
-                  } else {
-                    setZoomedIn(false);
-                  }
-                  }}
-                >
-                  <div className="wrapper">
-                    <img
-                      className={zoomedIn ? 'enlarged expanded-displayed' : 'expanded-displayed'}
-                      src={photo.url}
-                      alt="x"
-                    />
-                  </div>
-                  <div className="expanded-snapper">
-                    <a
-                      className={index === 0 ? 'nav-button disabled' : 'nav-button'}
-                      href={prev}
-                      onClick={(event) => { event.stopPropagation(); changeSelectedImgInx(index - 1)}}
-                    >
-                      <i
-                        className={index === 0 ? 'fa-solid fa-chevron-left fa-xl disabled-nav-button' : 'fa-solid fa-chevron-left fa-xl'}
-                      />
-                    </a>
-                    <a
-                      className={index === length - 1 ? 'nav-button disabled' : 'nav-button'}
-                      href={next}
-                      onClick={(event) => { event.stopPropagation(); changeSelectedImgInx(index + 1); }}
-                    >
-                      <i
-                        className={index === length - 1 ? 'fa-solid fa-chevron-right fa-xl disabled-nav-button' : 'fa-solid fa-chevron-right fa-xl'}
-                      />
-                    </a>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+      <div className="wrapper" onClick={() => {changeView(true)}}>
+        <div className="zoomImage">
+          <img
+            className={zoomedIn ? 'enlarged expanded-displayed-image' : 'expanded-displayed-image'}
+            src={selectedStyle.photos[selectedImageIndex].url}
+            alt="x"
+            onClick={(event)=> {
+              event.preventDefault();
+              if (!zoomedIn) {
+                setZoomedIn(true);
+              } else {
+                setZoomedIn(false);
+              }
+            }}
+          />
         </div>
-      </section> */}
+      </div>
       <div className="navigator">
         {selectedStyle.photos.map((photo, index) => (
           // eslint-disable-next-line jsx-a11y/control-has-associated-label
