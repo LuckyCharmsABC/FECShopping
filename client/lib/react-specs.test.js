2a24f84ef42c9906eb90feef4c18abec2470e824
Boyi Qu <boyiqu9596@gmail.com>
2022-10-25 17:44:35 -0700

    rewrote test files

--- client/lib/react-specs.test.js
+++ client/lib/react-specs.test.js
@@ -1,17 +1,15 @@
 import React from 'react';
+
 // import react-testing methods
 import {render, waitFor, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../src/components/App.jsx';
 
+describe('Example test', () => {
+  test('Displays Hello in App', async () => {
+    //render (<App />);
+    await waitFor(() => screen.getByRole('heading'));
 
-
-describe('Example test', ()=>{
-  test('Displays Hello in App', ()=>{
-    render (<App />);
-    waitFor(()=> screen.getByRole('heading'))
-    .then(()=>{
-      expect(screen.getByRole('heading')).toHaveTextContent('Hello')
-    })
+    expect(screen.getByRole('heading')).toHaveTextContent('Hello');
   })
 })
