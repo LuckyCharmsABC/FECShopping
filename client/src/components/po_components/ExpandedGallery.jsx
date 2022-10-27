1df8534ee85f3e80363a0827ff00eb12f36142eb
Boyi Qu <boyiqu9596@gmail.com>
2022-10-27 15:19:54 -0700

    added mouseWatch functionality, image will scroll when mouse moves

--- client/src/components/po_components/ExpandedGallery.jsx
+++ client/src/components/po_components/ExpandedGallery.jsx
@@ -5,34 +5,37 @@ const ExpandedGallery = ({
   selectedStyle, selectedImageIndex, changeSelectedImgInx, changeView,
 }) => {
   const [zoomedIn, setZoomedIn] = useState(false);
-/*   const zoomImage = (x, y) => {
-    document.getElementById('display-image').style.cursor = 'zoom-out';
-    document.getElementById('display-image').style.tranform = 'scale(2.5)';
-    document.getElementById('display-image').style['tranform-origin'] = `${x} ${y}`;
-    document.getElementById('display-image').style.transition = 'transform 0.25s ease';
-  }; */
+  const [mouseCoord, setMouseCoord] = useState({ x: 0, y: 0 });
+  const watchMouse = (xCoord, yCoord) => {
+    const newCoord = { ...mouseCoord, x: xCoord, y: yCoord };
+    setMouseCoord(newCoord);
+  };
 
   return (
     <div id="expanded-gallery">
-      <div className="wrapper" onClick={() => {changeView(true)}}>
+      <div className="wrapper" onClick={() => { changeView(true) }}>
         <div
           className="zoomImage"
+          id="container"
           role="button"
           onClick={(event) => {
             event.preventDefault();
+            const element = document.getElementById('display-image');
             if (!zoomedIn) {
-              const element = document.getElementById('display-image');
               const imageleft = element.getBoundingClientRect().x;
               const imagetop = element.getBoundingClientRect().y;
-              // console.log(`x is ${event.clientX}, y is ${event.clientY}`);
               element.style.setProperty('--x', `${event.clientX - imageleft}px`);
               element.style.setProperty('--y', `${event.clientY - imagetop}px`);
-              // console.log(getComputedStyle(element).getPropertyValue('transform-origin'));
               setZoomedIn(true);
             } else {
               setZoomedIn(false);
             }
           }}
+          onMouseMove={zoomedIn ? (event) => {
+            const element = document.getElementById('container');
+            watchMouse(event.clientX, event.clientY);
+            element.scrollBy(10, 0);
+          } : () => {}}
           onKeyPress={() => {}}
           tabIndex="0"
         >
@@ -41,20 +44,6 @@ const ExpandedGallery = ({
             className={zoomedIn ? 'enlarged expanded-displayed-image' : 'expanded-displayed-image'}
             src={selectedStyle.photos[selectedImageIndex].url}
             alt="x"
-/*             onClick={(event)=> {
-              event.preventDefault();
-              if (!zoomedIn) {
-                const element = document.getElementById('display-image');
-                console.log(`x is ${event.clientX}, y is ${event.clientY}`)
-                element.style.setProperty(`--x`, `${event.clientX}px`);
-                element.style.setProperty(`--y`, `${event.clientY}px`);
-                console.log(getComputedStyle(element).getPropertyValue('transform-origin'));
-                setZoomedIn(true);
-              } else {
-                setZoomedIn(false);
-              }
-            }}
-            onKeyPress={()=>{}} */
           />
         </div>
       </div>
