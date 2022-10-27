ed188bf8bf97a7f6aeb0a80570a11b703d9f1f32
Boyi Qu <boyiqu9596@gmail.com>
2022-10-27 10:30:25 -0700

    added features to additional info; restyled additional info

--- client/src/components/po_components/AdditionalInfo.jsx
+++ client/src/components/po_components/AdditionalInfo.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
-const AdditionalInfo = ({ product }) => (
-  <div className="additional-info">
-    <b>{product.slogan}</b>
-    <p>{product.description}</p>
-  </div>
-);
+const AdditionalInfo = ({ product }) => {
+  const renderFeatures = () => {
+    if (product.features) {
+      return (
+        <div className="AddInfo2">
+          <div className="divider-2" />
+          <div className="features">
+            <ul>
+              {product.features.map((feature) => (
+                <li key={feature.feature}>{`${feature.feature}: ${feature.value}`}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+    return <div />;
+  };
+
+  return (
+    <div className="additional-info">
+      <div className="slogan-section">
+        <b>{product.slogan}</b>
+        <p>{product.description}</p>
+      </div>
+      {/* <div className="divider-2" />
+      <div className="features">
+        <ul>
+          {product.features.map((feature) => (
+            <li>{`${feature.feature}: ${feature.value}`}</li>
+          ))}
+        </ul>
+      </div> */}
+      {renderFeatures()}
+    </div>
+  );
+};
 
 export default AdditionalInfo;
