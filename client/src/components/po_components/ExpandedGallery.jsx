9796426d439558949688080bf6e63d7002da8b32
Boyi Qu <boyiqu9596@gmail.com>
2022-10-27 16:46:37 -0700

    removed commented out css; modified expandedgallery to comply with linter

--- client/src/components/po_components/ExpandedGallery.jsx
+++ client/src/components/po_components/ExpandedGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import ExpandedViewButtons from './ExpandedViewButtons.jsx';
 
 const ExpandedGallery = ({
